5eeeba7073f845186a05a6e95b5df7e6c49313fc
Martin Vesterlund <Otyg@users.noreply.github.com>
2021-10-18 11:52:14 +0200

    Change dockerfile

---
+++ build-threagile.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export GOOS=linux
+go mod download
+go version
+go build -a -trimpath -ldflags="-s -w -X main.buildTimestamp=$(date '+%Y%m%d%H%M%S')" -gcflags="all=-trimpath=/src" -asmflags="all=-trimpath=/src" -buildmode=plugin -o raa.so raa/raa/raa.go
+go build -a -trimpath -ldflags="-s -w -X main.buildTimestamp=$(date '+%Y%m%d%H%M%S')" -gcflags="all=-trimpath=/src" -asmflags="all=-trimpath=/src" -buildmode=plugin -o dummy.so raa/dummy/dummy.go
+go build -a -trimpath -ldflags="-s -w -X main.buildTimestamp=$(date '+%Y%m%d%H%M%S')" -gcflags="all=-trimpath=/src" -asmflags="all=-trimpath=/src" -buildmode=plugin -o demo-rule.so risks/custom/demo/demo-rule.go
+go build -a -trimpath -ldflags="-s -w -X main.buildTimestamp=$(date '+%Y%m%d%H%M%S')" -gcflags="all=-trimpath=/src" -asmflags="all=-trimpath=/src" -o threagile
+go build -a -trimpath -ldflags="-s -w -X main.buildTimestamp=$(date '+%Y%m%d%H%M%S')" -gcflags="all=-trimpath=/src" -asmflags="all=-trimpath=/src" -buildmode=plugin -o missing-monitoring-rule.so custom/missing-monitoring/missing-monitoring-rule.go
+go build -a -trimpath -ldflags="-s -w -X main.buildTimestamp=$(date '+%Y%m%d%H%M%S')" -gcflags="all=-trimpath=/src" -asmflags="all=-trimpath=/src" -buildmode=plugin -o accidental-logging-of-sensitive-data-rule.so custom/accidental-logging-of-sensitive-data/accidental-logging-of-sensitive-data-rule.go
+go build -a -trimpath -ldflags="-s -w -X main.buildTimestamp=$(date '+%Y%m%d%H%M%S')" -gcflags="all=-trimpath=/src" -asmflags="all=-trimpath=/src" -buildmode=plugin -o missing-audit-of-sensitive-asset-rule.so custom/missing-audit-of-sensitive-asset/missing-audit-of-sensitive-asset-rule.go
+go build -a -trimpath -ldflags="-s -w -X main.buildTimestamp=$(date '+%Y%m%d%H%M%S')" -gcflags="all=-trimpath=/src" -asmflags="all=-trimpath=/src" -buildmode=plugin -o credential-stored-outside-of-vault-rule.so custom/credential-stored-outside-of-vault/credential-stored-outside-of-vault.go
+go build -a -trimpath -ldflags="-s -w -X main.buildTimestamp=$(date '+%Y%m%d%H%M%S')" -gcflags="all=-trimpath=/src" -asmflags="all=-trimpath=/src" -buildmode=plugin -o insecure-handling-of-sensitive-data-rule.so custom/insecure-handling-of-sensitive-data/insecure-handling-of-sensitive-data.go
