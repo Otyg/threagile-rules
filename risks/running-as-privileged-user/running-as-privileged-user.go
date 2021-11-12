8fb99677a0b6d23b94c2b0ff15c28b75c6193147
Martin Vesterlund <Otyg@users.noreply.github.com>
2021-11-12 09:18:51 +0100

    Finish rule

--- risks/running-as-privileged-user/running-as-privileged-user.go
+++ risks/running-as-privileged-user/running-as-privileged-user.go
@@ -21,8 +21,8 @@ func (r runningAsPrivilegedUser) Category() model.RiskCategory {
 		Check:                      "Referenced ASVS and cheat sheet",
 		Function:                   model.Operations,
 		STRIDE:                     model.ElevationOfPrivilege,
-		DetectionLogic:             "Technical assets without any of the non-privileged supported tags or with any of the privileged tags are flagged.",
-		RiskAssessment:             "Impact is based on criticality of the asset",
+		DetectionLogic:             "Technical assets without any of the supported tags flagged.",
+		RiskAssessment:             "Severity is based on the RAA of the asset",
 		FalsePositives:             "Running as root inside a container where the host remaps the user to a non-privileged one is a false positive.",
 		ModelFailurePossibleReason: false,
 		CWE:                        250,
@@ -30,58 +30,42 @@ func (r runningAsPrivilegedUser) Category() model.RiskCategory {
 }
 
 func (r runningAsPrivilegedUser) SupportedTags() []string {
-	return []string{"root", "non-root", "privileged", "unprivileged", "isAdmin", "isNotAdmin"}
+	return []string{"non-root", "unprivileged", "isNotAdmin"}
 }
 
 func (r runningAsPrivilegedUser) GenerateRisks() []model.Risk {
 	risks := make([]model.Risk, 0)
-	for _, data := range model.DataAssetsTaggedWithAny(r.SupportedTags()...) {
-		// credential-lifetime:unlimited set as default
-		exploitationImpact := model.MediumImpact
-		exploitationProbability := model.Frequent
-		dataBreachProbability := model.Probable
-		if data.IsTaggedWithAny("credential-lifetime:unknown/hardcoded") || !data.IsTaggedWithAny("credential-lifetime:unlimited", "credential-lifetime:long", "credential-lifetime:short") {
-			// If only credential-tag is present, assume unknown
-			exploitationImpact = model.HighImpact
-		} else if data.IsTaggedWithAny("credential-lifetime:long") {
-			exploitationProbability = model.VeryLikely
-		} else if data.IsTaggedWithAny("credential-lifetime:short") {
-			exploitationProbability = model.Likely
+	for _, id := range model.SortedTechnicalAssetIDs() {
+		techAsset := model.ParsedModelRoot.TechnicalAssets[id]
+		if techAsset.OutOfScope {
+			continue
 		}
-		if data.IsTaggedWithAny("credential-lifetime:manual-rotation", "credential-lifetime:auto-rotation") && !data.IsTaggedWithAny("credential-lifetime:unknown/hardcoded") {
-			exploitationImpact = exploitationImpact - 1
-			exploitationProbability = exploitationProbability - 1
-			dataBreachProbability = model.Possible
-		}
-		for _, technicalAsset := range data.StoredByTechnicalAssetsSorted() {
-			if technicalAsset.OutOfScope || technicalAsset.Technology == model.Vault {
-				continue
+		if !techAsset.IsTaggedWithAny(CustomRiskRule.SupportedTags()...) {
+			var impact = model.MediumImpact
+			var likelihood = model.Likely
+			if techAsset.RAA < 0.2 {
+				impact = model.LowImpact
+				likelihood = model.Unlikely
+			} else if techAsset.RAA > 0.8 {
+				impact = model.HighImpact
+				likelihood = model.VeryLikely
 			}
-			if technicalAsset.Confidentiality == model.StrictlyConfidential && technicalAsset.Encryption != model.NoneEncryption {
-				// Assume that a technical asset classed for Strictly Confidential is well protected
-				if exploitationProbability > model.Unlikely {
-					exploitationProbability = exploitationProbability - 1
-				}
-				dataBreachProbability = model.Improbable
-			}
-			risks = append(risks, createRisk(technicalAsset, exploitationImpact, exploitationProbability, data.Id, dataBreachProbability))
+			risks = append(risks, createRisk(techAsset, impact, likelihood))
 		}
 	}
 	return risks
 }
 
-func createRisk(technicalAsset model.TechnicalAsset, impact model.RiskExploitationImpact, probability model.RiskExploitationLikelihood, mostCriticalDataId string, dataProbability model.DataBreachProbability) model.Risk {
+func createRisk(technicalAsset model.TechnicalAsset, impact model.RiskExploitationImpact, likelihood model.RiskExploitationLikelihood) model.Risk {
 	title := "<b>Credential stored outside of vault</b> risk at <b>" + technicalAsset.Title + "</b>"
 	risk := model.Risk{
 		Category:                     CustomRiskRule.Category(),
-		Severity:                     model.CalculateSeverity(probability, impact),
-		ExploitationLikelihood:       probability,
+		Severity:                     model.CalculateSeverity(likelihood, impact),
+		ExploitationLikelihood:       likelihood,
 		ExploitationImpact:           impact,
 		Title:                        title,
 		MostRelevantTechnicalAssetId: technicalAsset.Id,
-		MostRelevantDataAssetId:      mostCriticalDataId,
-		DataBreachProbability:        dataProbability,
-		DataBreachTechnicalAssetIDs:  []string{technicalAsset.Id},
+		DataBreachTechnicalAssetIDs:  []string{},
 	}
 	risk.SyntheticId = risk.Category.Id + "@" + technicalAsset.Id
 	return risk
