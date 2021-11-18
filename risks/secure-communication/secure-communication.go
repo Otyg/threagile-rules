0230422389dbc153b52de565213f9e0c215d7dd0
Martin Vesterlund <Otyg@users.noreply.github.com>
2021-11-18 09:21:47 +0100

    Finish up

--- risks/secure-communication/secure-communication.go
+++ risks/secure-communication/secure-communication.go
@@ -10,7 +10,7 @@ var CustomRiskRule secureCommunication
 
 func (r secureCommunication) Category() model.RiskCategory {
 	return model.RiskCategory{
-		Id:                         "insecure-communication",
+		Id:                         "use-of-weak-cryptography-in-transit",
 		Title:                      "Use Of Weak Cryptography in transit",
 		Description:                "To ensure confidentiality during transit strong encryption must be used; weak, broken or soon to be deprecated algorithms must be avoided and recommended key lengths must be applied.",
 		Impact:                     "Weak cryptography can result in information disclosure and a false sense of security.",
@@ -33,25 +33,56 @@ func (r secureCommunication) SupportedTags() []string {
 }
 func (r secureCommunication) GenerateRisks() []model.Risk {
 	risks := make([]model.Risk, 0)
-	for _, comm := range model.CommunicationLinks {
-		if comm.Protocol.IsEncrypted() {
-			//TODO: Add logic for finding most critical data asset and base impact on that
-			risks = append(risks, createRisk(model.ParsedModelRoot.TechnicalAssets[comm.SourceId], comm))
+	for _, technicalAsset := range model.ParsedModelRoot.TechnicalAssets {
+		var mostCriticalCommlink model.CommunicationLink
+		var hasEncryptedComLinks = false
+		for _, comm := range technicalAsset.CommunicationLinksSorted() {
+			if comm.Protocol.IsEncrypted() {
+				hasEncryptedComLinks = true
+				if comm.HighestConfidentiality() > mostCriticalCommlink.HighestConfidentiality() {
+					mostCriticalCommlink = comm
+				}
+			}
+		}
+		if hasEncryptedComLinks {
+			var mostCriticalDataAsset model.DataAsset
+			for _, data := range append(mostCriticalCommlink.DataAssetsSentSorted(), mostCriticalCommlink.DataAssetsReceivedSorted()...) {
+				if data.Confidentiality > mostCriticalDataAsset.Confidentiality {
+					mostCriticalDataAsset = data
+				}
+			}
+			var exploitationImpact model.RiskExploitationImpact
+			switch mostCriticalDataAsset.Confidentiality {
+			case model.Public:
+				exploitationImpact = model.LowImpact
+			case model.Internal:
+				exploitationImpact = model.LowImpact
+			case model.Restricted:
+				exploitationImpact = model.MediumImpact
+			case model.Confidential:
+				exploitationImpact = model.HighImpact
+			case model.StrictlyConfidential:
+				exploitationImpact = model.VeryHighImpact
+			}
+			risks = append(risks, createRisk(technicalAsset, mostCriticalCommlink, mostCriticalDataAsset, exploitationImpact))
 		}
 	}
 	return risks
 }
 
-func createRisk(technicalAsset model.TechnicalAsset, commLink model.CommunicationLink) model.Risk {
-	title := "<b>Use of weak cryptography</b> risk at <b>" + technicalAsset.Title + "</b>"
+func createRisk(technicalAsset model.TechnicalAsset, commLink model.CommunicationLink, dataAsset model.DataAsset, exploitationImpact model.RiskExploitationImpact) model.Risk {
+	title := "<b>Use of weak cryptography in transit</b> risk at <b>" + technicalAsset.Title + "</b>"
 	risk := model.Risk{
 		Category:                        CustomRiskRule.Category(),
-		Severity:                        model.CalculateSeverity(model.Unlikely, model.MediumImpact),
+		Severity:                        model.CalculateSeverity(model.Unlikely, exploitationImpact),
 		ExploitationLikelihood:          model.Unlikely,
-		ExploitationImpact:              model.MediumImpact,
+		ExploitationImpact:              exploitationImpact,
 		Title:                           title,
 		MostRelevantTechnicalAssetId:    technicalAsset.Id,
 		MostRelevantCommunicationLinkId: commLink.Id,
+		MostRelevantDataAssetId:         dataAsset.Id,
+		DataBreachTechnicalAssetIDs:     []string{technicalAsset.Id},
+		DataBreachProbability:           model.Possible,
 	}
 	risk.SyntheticId = risk.Category.Id + "@" + commLink.Id
 	return risk
